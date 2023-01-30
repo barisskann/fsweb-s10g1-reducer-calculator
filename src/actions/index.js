4336decec25068cc40757b9a0e59298e4dd4e6a3
barisskann <bars_958@hotmail.com>
2023-01-30 12:23:54 +0300

    S10-G1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DSPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const MR = "MR";
+export const MC = "MC";
+
+export const MPLUS = "MPLUS";
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
